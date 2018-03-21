cb8a44f154844ee495822e0ae74be7d94e560508
you@example.com <you@example.com>
2018-03-21 17:50:27 +0100

    Zajecia

---
+++ POLab4/src/main/java/Kwadrat.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author student
+ */
+public class Kwadrat {
+    
+    private double a;
+    private double b;
+    public Kwadrat(double a, double b){
+        this.a=a;
+        this.b=b;
+    }
+    public void obliczPole(){
+        System.out.println(Math.pow(a, 2));
+    }
+    
+    
+}
